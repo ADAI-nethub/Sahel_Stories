1e7937ad423430c16c3ab834e9708747e154428e
ADAI-nethub <atomosdubaako@gmail.com>
2025-08-04 14:14:01 +0100

    Prepare for deployment

--- config/settings.py
+++ config/settings.py
@@ -96,7 +96,7 @@ USE_TZ = True
 # Static files
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 
 # Security settings (production only)
 if not DEBUG:
